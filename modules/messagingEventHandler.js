1f42feae8750037da9d0d9a869c0b87b8146009f
John Workmeister <jworkmeister@brunnerworks.com>
2016-10-17 15:20:38 -0400

    still working

--- modules/messagingEventHandler.js
+++ modules/messagingEventHandler.js
@@ -97,6 +97,28 @@ module.exports = (function(){
       }
     },
 
+
+    receivedAuthentication: function(event) {
+      var senderID = event.sender.id;
+      var recipientID = event.recipient.id;
+      var timeOfAuth = event.timestamp;
+
+      // The 'ref' field is set in the 'Send to Messenger' plugin, in the 'data-ref'
+      // The developer can set this to an arbitrary value to associate the 
+      // authentication callback with the 'Send to Messenger' click event. This is
+      // a way to do account linking when the user clicks the 'Send to Messenger' 
+      // plugin.
+      var passThroughParam = event.optin.ref;
+
+      console.log("Received authentication for user %d and page %d with pass " +
+        "through param '%s' at %d", senderID, recipientID, passThroughParam, 
+        timeOfAuth);
+
+      // When an authentication is received, we'll send a message back to the sender
+      // to let them know it was successful.
+      sender.sendTextMessage(senderID, "Authentication successful");
+    },
+
     onDeliveryConfirmation: function(event){
       var senderID       = event.sender.id;
       var recipientID    = event.recipient.id;
@@ -141,7 +163,7 @@ module.exports = (function(){
 
       console.log("Received message read event for watermark %d and sequence " +
         "number %d", watermark, sequenceNumber);
-    },
+},
 
     onAccountLinking: function(event){
       var senderID = event.sender.id;
@@ -152,7 +174,7 @@ module.exports = (function(){
 
       console.log("Received account link event with for user %d with status %s " +
         "and auth code %s ", senderID, status, authCode);
-    }
+}
   };
 
   return messagingEventHandler;
--- modules/routes.js
+++ modules/routes.js
@@ -73,6 +73,27 @@ module.exports = function (app){
 		}
 	});
 
+function receivedAuthentication(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfAuth = event.timestamp;
+
+    // The 'ref' field is set in the 'Send to Messenger' plugin, in the 'data-ref'
+    // The developer can set this to an arbitrary value to associate the 
+    // authentication callback with the 'Send to Messenger' click event. This is
+    // a way to do account linking when the user clicks the 'Send to Messenger' 
+    // plugin.
+    var passThroughParam = event.optin.ref;
+
+    console.log("Received authentication for user %d and page %d with pass " +
+        "through param '%s' at %d", senderID, recipientID, passThroughParam, 
+        timeOfAuth);
+
+    // When an authentication is received, we'll send a message back to the sender
+    // to let them know it was successful.
+    sendTextMessage(senderID, "Authentication successful");
+}
+
 	app.get("/authorize", function(req, res){
 		var accountLinkingToken = req.query.account_linking_token;
 		var redirectURI = req.query.redirect_uri;
